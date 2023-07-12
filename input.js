8ee847f08242f3b48bc0e66c4736b8de45ee775c
Mandy Liu <manling512@gmail.com>
2023-07-12 21:52:48 +0000

    Manage the user input.

---
+++ input.js
@@ -0,0 +1,31 @@
+let connection;
+const setupInput = (conn) => {
+  connection = conn;
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function (key) {
+  if (key == 'w') {
+    connection.write("Move: up");
+  }
+  if (key == 'd') {
+    connection.write("Move: right");
+  }
+  if (key == 's') {
+    connection.write("Move: down");
+  }
+  if (key == 'a') {
+    connection.write("Move: left");
+  }
+  if (key == 'm') {
+    connection.write("Say: HELLO");
+  }
+  if (key == '\u0003') { process.exit();}
+};
+
+module.exports = { setupInput };
